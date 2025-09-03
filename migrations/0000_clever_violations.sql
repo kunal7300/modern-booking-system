759785c0a356e46765e18eaf9c4862e7342375f9
kunal sharma <renintubule@gmail.com>
2025-09-03 23:00:24 +0530

    Delete migrations directory

--- migrations/0000_clever_violations.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `usersImage` (
-	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
-	`userId` text NOT NULL,
-	`image` text NOT NULL,
-	`name` text NOT NULL,
-	`description` text NOT NULL,
-	`created_at` text DEFAULT (CURRENT_TIMESTAMP) NOT NULL
-);
