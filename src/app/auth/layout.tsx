b543aa217fef57e2e37f55343b7e3b4f7a4488f5
kunal sharma <renintubule@gmail.com>
2025-09-03 23:00:58 +0530

    Delete src/app directory

--- src/app/auth/layout.tsx
+++
@@ -1,7 +0,0 @@
-const RootLayout = ({ children }: { children: React.ReactNode }) => {
-    return (
-        <main className="flex justify-center items-center mt-24">{children}</main>
-    )
-}
-
-export default RootLayout;
